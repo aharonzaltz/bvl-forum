a09e748aea1e5dd28b84176315d290e9cf05e8d2
aharon_anopa <aharon@anopa.co.il>
2023-07-12 18:01:21 +0300

    install

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.3.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = 'db-mysql-nyc3-80194-do-user-14369143-0.b.db.ondigitalocean.com';
+$dbport = '25060';
+$dbname = 'defaultdb';
+$dbuser = 'doadmin';
+$dbpasswd = 'AVNS_g7zha70FthHeirJAJ2j';
+$table_prefix = 'phpbb_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
